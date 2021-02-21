84bc951270220969fcf298785905506d68c87df2
Milan Mody <milanm1120@gmail.com>
2021-02-21 05:49:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,14 @@
 #write your code here
 
-def countdown(seconds)
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
-    seconds -= 1
-  end
-  Return "HAPPY NEW YEAR!"
-end
-
-countdown(20)
+# def countdown(seconds)
+#   while seconds > 0
+#     puts "#{seconds} SECOND(S)!"
+#     seconds -= 1
+#   end
+#   Return "HAPPY NEW YEAR!"
+# end
+#
+# countdown(20)
 
 
 def countdown_with_sleep(seconds)
