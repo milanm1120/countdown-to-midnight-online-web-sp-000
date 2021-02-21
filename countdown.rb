09dd1c8834021fcf68c0c3209d18eccfb2e395df
Milan Mody <milanm1120@gmail.com>
2021-02-21 05:41:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,1 +1,11 @@
 #write your code here
+
+def countdown(number)
+  counter = 10
+  while
+    counter > 0
+      puts "#{number} SECOND(S)!"
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
