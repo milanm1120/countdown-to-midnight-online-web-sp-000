0fbdf294da4dcd817e9bced3a6abd6237a4c2254
Milan Mody <milanm1120@gmail.com>
2021-02-21 05:49:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,15 @@ def countdown(seconds)
 end
 
 countdown(20)
+
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  Return "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(20)
